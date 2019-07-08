d7955d2f454cde7e52249ec6a2c1eb7393e62908
jocelo <gopher@gmail.com>
2019-07-07 23:49:16 -0400

    is_unique

--- array_and_strings/is_unique.js
+++ array_and_strings/is_unique.js
@@ -1,19 +1,47 @@
-// todo: create module to track timing (performance)
+// Exercise 1.1
+// validate if a string has all unique characters
 
-// a string has all unique characters
+// solution 1:
+// Creat a hash table that holds each element one by one
+// if a duplicate is found, the function is halted
 
-// solution 1
-function main_function(token) {
+creating_hash = (stringToValidate) => {
 	// over ascii char length
-	if (token.length > 255) {
+	if (stringToValidate.length > 255) {
 		return false;
 	}
+
+	const letters = {};
 	let i=0;
-	for ( ; i<token.length ; i++) {
-		console.log(token[i]);
+	for ( ; i<stringToValidate.length ; i++) {
+		if (letters[stringToValidate[i]]) {
+			return false;
+		}
+		letters[stringToValidate[i]] = true;
 	}
 
-	return false;
+	return true;
 }
 
-module.exports = main_function;
+// solution 2
+// comparing each character of the string agains the rest of them
+comparing_one_by_one = (string) => {
+	let i = 0;
+	let j = 0;
+
+	const sizeOfString = string.length;
+	
+	for ( ; i<sizeOfString ; i++ ) {
+		let token = string[i];
+		j = i+1;
+		// the rest of the elements on the string
+		for ( ; j<sizeOfString ; j++) {
+			if (token === string[j]) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+module.exports = comparing_one_by_one;
--- is_unique.test.js
+++ is_unique.test.js
@@ -1,6 +1,36 @@
 const is_unique = require("./array_and_strings/is_unique.js");
 
-const oneStr = 'abcgeyqosmjn';
-test(`Evaluating string: `, () => {
-	expect(is_unique(oneStr)).toBe(true);
+let testString = 'abc';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
 })
+
+testString = 'abcdefghijklmnopqrstuvwxyz';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
+});
+
+testString = 'a b c d';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(false);
+});
+
+testString = 'a bcd';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
+});
+
+testString = 'a bcd';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
+});
+
+testString = ' ';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
+});
+
+testString = '';
+test(`Evaluating string: ${testString}`, () => {
+	expect(is_unique(testString)).toBe(true);
+});
